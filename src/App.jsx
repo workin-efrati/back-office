eaee4d52b47919fe848dcdb4c4ff01036aba061b
yosefus <yosalsoni@gmail.com>
2024-07-21 10:21:23 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Yosef from "./tests/Yosef"
+
 
 export default function App() {
+  const router = createBrowserRouter([{
+    path: 'test',
+    children: [
+      {
+        path: 'yosef',
+        element: <Yosef />
+      }
+    ]
+  },
+  {
+    index: true,
+    element: <div>good luck!</div>
+  }
+  ])
+
   return (
-    <div>App</div>
+    <div>
+      <RouterProvider router={router} />
+    </div>
   )
 }
